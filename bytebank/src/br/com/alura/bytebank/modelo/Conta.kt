2e4fb5c5fd6241cdc656196a69c59c99e8220dfa
Matheus Oliveira <matheusolliveiira@hotmail.com>
2022-01-21 23:43:52 -0300

    atualizando

--- bytebank/src/br/com/alura/bytebank/modelo/Conta.kt
+++ bytebank/src/br/com/alura/bytebank/modelo/Conta.kt
@@ -6,6 +6,15 @@ abstract class Conta(
 ) {
     var saldo = 0.0
         protected set
+    companion object {
+        var total = 0
+            private set
+    }
+
+    init {
+        println("Criando conta")
+        total++
+    }
 
     fun deposita(valor: Double) {
         if (valor > 0) {
@@ -22,5 +31,34 @@ abstract class Conta(
             return true
         }
         return false
+    }
+}
+
+class ContaCorrente(
+    titular: Cliente,
+    numero: Int
+) : Conta(
+    titular = titular,
+    numero = numero
+) {
+    override fun saca(valor: Double) {
+        val valorComTaxa = valor + 0.1
+        if(this.saldo >= valorComTaxa){
+            this.saldo -= valorComTaxa
+        }
+    }
+}
+
+class ContaPoupanca(
+    titular: Cliente,
+    numero: Int
+) : Conta(
+    titular = titular,
+    numero = numero
+) {
+    override fun saca(valor: Double) {
+        if(this.saldo >= valor){
+            this.saldo -= valor
+        }
     }
 }
