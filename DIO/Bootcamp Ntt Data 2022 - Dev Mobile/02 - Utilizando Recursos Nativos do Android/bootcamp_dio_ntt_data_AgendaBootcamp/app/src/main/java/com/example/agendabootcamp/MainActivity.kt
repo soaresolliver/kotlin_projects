9c7f164fce850807f76b8339ad1428899ffe9291
Matheus Oliveira <matheusolliveiira@hotmail.com>
2022-02-01 19:12:05 -0300

    adicionando arquivos

---
+++ DIO/Bootcamp Ntt Data 2022 - Dev Mobile/02 - Utilizando Recursos Nativos do Android/bootcamp_dio_ntt_data_AgendaBootcamp/app/src/main/java/com/example/agendabootcamp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.agendabootcamp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.CalendarContract
+import android.provider.CalendarContract.Events.*
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+
+        val btnSetEvent = findViewById<Button>(R.id.set_event)
+
+        btnSetEvent.setOnClickListener {
+
+            val intent = Intent(Intent.ACTION_INSERT)
+                .setData(CONTENT_URI)
+                .putExtra(TITLE, "Bootcamp everis")
+                .putExtra(EVENT_LOCATION, "online")
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, System.currentTimeMillis())
+                .putExtra(
+                    CalendarContract.EXTRA_EVENT_END_TIME,
+                    System.currentTimeMillis() + (60 * 60 * 1000)
+                )
+            startActivity(intent)
+        }
+    }
+}
---
+++ DIO/Bootcamp Ntt Data 2022 - Dev Mobile/02 - Utilizando Recursos Nativos do Android/bootcamp_dio_ntt_data_Contatos/app/src/main/java/com/example/contatosbootcamp/Contact.kt
@@ -0,0 +1,5 @@
+package com.example.contatosbootcamp
+
+class Contact(val name: String, val phoneNumber: String) {
+
+}
