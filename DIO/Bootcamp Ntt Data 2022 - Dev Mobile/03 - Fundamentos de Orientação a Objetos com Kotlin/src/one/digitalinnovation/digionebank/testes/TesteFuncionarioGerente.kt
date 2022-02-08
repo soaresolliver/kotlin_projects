0ab2a19549d5ccf69a112addd8b561d0e4fb13e7
Matheus Oliveira <matheusolliveiira@hotmail.com>
2022-02-08 13:40:25 -0300

    created tests and report printer

---
+++ DIO/Bootcamp Ntt Data 2022 - Dev Mobile/03 - Fundamentos de Orientação a Objetos com Kotlin/src/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Gerente
+
+fun main() {
+    val maria = Gerente("Maria do Carmo", "1234567809", 5000.0)
+    ImprimeRelatorioFuncionario.imprime(maria)
+}
